830d0d6bf82e7f4bf928899aa12954ff0b26abab
Yann Christophe <77779939+Yann0411@users.noreply.github.com>
2024-01-23 14:34:11 +0100

    to do list

---
+++ app.js
@@ -0,0 +1,48 @@
+const newTaskInput = document.getElementById('new-task');
+const addTaskButton = document.getElementById('add-task');
+const taskList = document.getElementById('task-list');
+const myCheckbox = document.querySelector('.checkboxId')
+
+
+
+addTaskButton.addEventListener('click', () => {
+    
+    const li = document.createElement('li')
+    li.classList.add('task-list--li')
+
+    const mycheckboxInput = document.createElement('input')
+    mycheckboxInput.classList.add('checkboxId')
+    mycheckboxInput.type = "checkbox"
+
+    mycheckboxInput.addEventListener('change',checkbox)
+    function checkbox(){
+
+        if(mycheckboxInput.checked){
+    
+            li.classList.toggle('completed')
+    
+        }else{
+            li.classList.remove('completed')
+        }
+    
+    }
+    
+    // li.append(mycheckboxInput)
+
+    const myLabel = document.createElement('label')
+    // myLabel.htmlFor = "checkboxId"
+    myLabel.textContent = newTaskInput.value
+    myLabel.appendChild(mycheckboxInput)
+    li.append(myLabel)
+
+
+    taskList.append(li)
+    newTaskInput.value = " "
+
+  
+
+   
+
+})
+
+
