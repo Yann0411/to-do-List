bd5f3055e7904c6a26a53a8dc497aa5e6b8dc0ed
Yann Christophe <77779939+Yann0411@users.noreply.github.com>
2024-01-26 20:32:17 +0100

    ajout de fonctionalitée

--- app.js
+++ app.js
@@ -1,54 +1,75 @@
-const newTaskInput = document.getElementById('new-task');
-const addTaskButton = document.getElementById('add-task');
-const taskList = document.getElementById('task-list');
-const myCheckbox = document.querySelector('.checkboxId')
+const inputVal = document.querySelector('.inputValider')
+const ul = document.querySelector('.ulList')
+const ulListCompleted = document.querySelector('.ulListCompleted')
+const ulListCancel = document.querySelector('.ulListCancel')
+const button = document.querySelector('button')
+
+let monStockage = []
+button.addEventListener('click', (label) => {
+
+    if (inputVal.value.trim() === "") {
+        alert('renseigner le champs')
+    } else {
+
+        const li = document.createElement('li')
 
 
+        const checkbox = document.createElement('input')
+        checkbox.classList.add('checkbox')
+        checkbox.type = "checkbox"
+        li.append(checkbox)
 
-addTaskButton.addEventListener('click', () => {
-    
-    const li = document.createElement('li')
-    li.classList.add('task-list--li')
+        const label = document.createElement('label')
+        label.htmlFor = 'label'
 
-    const mycheckboxInput = document.createElement('input')
-    mycheckboxInput.classList.add('checkboxId')
-    mycheckboxInput.type = "checkbox"
+        label.textContent = inputVal.value
+        monStockage.push(inputVal.value)
+        localStorage.setItem("ToDo--List", JSON.stringify(monStockage))
 
-    mycheckboxInput.addEventListener('change',checkbox)
-    function checkbox(){
+        li.append(label)
 
-        if(mycheckboxInput.checked){
-    
-            li.classList.toggle('completed')
-    
-        }else{
-            li.classList.remove('completed')
-        }
-    
+        ul.append(li)
+
+        inputVal.value = ""
+
+        checkbox.addEventListener('change', (event) => {
+            if (event.target.checked) {
+                // li.classList.toggle('completed')
+                const liCompleted = document.createElement('li')
+                ulListCompleted.append(liCompleted)
+                liCompleted.textContent = label.textContent
+
+
+            } 
+
+
+        })
+
+        li.addEventListener('dblclick', () => {
+            console.log(li)
+            const liCancelled = document.createElement('li');
+            ulListCancel.append(liCancelled); // Assurez-vous que ulListCancel est correctement sélectionné
+            liCancelled.textContent = label.textContent;
+        
+            // Supprimer l'élément de liste de la liste actuelle
+             li.remove();
+        });
+
     }
-    
-    // li.append(mycheckboxInput)
-
-    const myLabel = document.createElement('label')
-    // myLabel.htmlFor = "checkboxId"
-    if(newTaskInput.value.trim() === ""){
-        alert("renseigner le champs")
-    }else{
-        myLabel.textContent = newTaskInput.value
-    }
-
-  
-    myLabel.appendChild(mycheckboxInput)
-    li.append(myLabel)
-
-
-    taskList.append(li)
-    newTaskInput.value = " "
-
-  
-
-   
 
 })
 
 
+
+
+
+// const animal = {
+//     monChien : "Berger",
+// }
+
+
+// function saveInStorage(){
+//     localStoragestorage.setItem("animal", JSON.stringify(animal));
+// }
+
+// saveInStorage()
